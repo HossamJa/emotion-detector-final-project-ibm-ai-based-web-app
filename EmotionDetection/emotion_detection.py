7b62f1a3302102521ff4ea050e4cee22a46ce1f4
HossamJa <jamhosam123@gmail.com>
2025-06-26 15:03:38 -0400

    completed Task 7: Incorporate Error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,18 +8,27 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
     formated_response = json.loads(response.text)  # Return the response text from the API
     
-    anger_score = formated_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formated_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formated_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formated_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formated_response['emotionPredictions'][0]['emotion']['sadness']
-    
-    emotion_scores = {'anger': float(anger_score), 'disgust': float(disgust_score), 'fear': float(fear_score), 'joy': float(joy_score), 'sadness': float(sadness_score)}
+    if response.status_code == 200:
+        anger_score = formated_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formated_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formated_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formated_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formated_response['emotionPredictions'][0]['emotion']['sadness']
+        
+        emotion_scores = {'anger': float(anger_score), 'disgust': float(disgust_score), 'fear': float(fear_score), 'joy': float(joy_score), 'sadness': float(sadness_score)}
 
-    dominant_emotion_score = sorted(emotion_scores.values())[-1]
-    for key, val in emotion_scores.items():
-        if val == dominant_emotion_score:
-            dominant_emotion = key
+        dominant_emotion_score = sorted(emotion_scores.values())[-1]
+        for key, val in emotion_scores.items():
+            if val == dominant_emotion_score:
+                dominant_emotion = key
+    
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
 
     return {
 'anger': anger_score,
